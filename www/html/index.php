a0cba88dbcc4182762036c8502bdaedfae31c820
cetanhota <wayne.leutwler@gmail.com>
2024-01-07 18:46:04 -0500

    new index

--- www/html/index.php
+++ www/html/index.php
@@ -16,8 +16,6 @@ $sql = "select tmp from percona order by ts desc limit 1;";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 	echo "<table align=center style='font-size:26px; color:#005266; text-shadow: 1px 1px 1px #b8b894;'><th> Temperature </th></tr>";
-     //echo "<table style='box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); border: 1px solid black;' border=1 align=center bgcolor='white'><tr><th> Tempeture </th><th> Humidity </th><th> Dew Point </th><th> Heat Index </th><th> Pressure </th><th> UV </th><th>Date</th></tr>";
-     // output data of each row
      while($row = $result->fetch_assoc()) {
          echo "<tr><td align=center>" . $row["tmp"]. "</td></tr>";
      }
@@ -31,7 +29,6 @@ $conn->close();
 </tr>
 </table> 
 
-
 </h2></center>
 <table align=center>
 <td><a href="http://weather.com" class="f_button">The Weather Channel</a>
@@ -41,6 +38,24 @@ $conn->close();
 <td><a href="http://nbc4i.com/category/weather/" class="f_button">Storm Team 4</a></td>
 </table>
 <center> 
+
+<?php
+$sql = "SELECT ts FROM weather.percona WHERE ts <= current_timestamp() order by ts desc limit 1;";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	echo "<table align=center><th> Last DB update time:</th></tr>";
+     while($row = $result->fetch_assoc()) {
+         echo "<tr><td align=center>" . $row["ts"]. "</td></tr>";
+     }
+     echo "</table>";
+} else {
+     echo "0 results";
+}
+$conn->close();
+?>
+</td>
+</tr>
+</table> 
 <br>
 <br>
 <?php include ('include/footer.inc.php');
